eddda41cffe9671ca0511fd9e786c7afdef34825
saikat20034 <mazharulislamsaikat2000@gmail.com>
2025-05-14 13:17:11 +0600

    Background ,animation and pagination change in Add Visa page

--- src/Pages/AllVisas/AllVisas.jsx
+++ src/Pages/AllVisas/AllVisas.jsx
@@ -109,13 +109,13 @@ const AllVisas = () => {
         <span className="text-blue-800 font-semibold">
           Page {currentPage} of {totalPages}
         </span>
-        <button
+        {/* <button
           onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
           disabled={currentPage === totalPages}
           className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:opacity-50"
         >
           Next
-        </button>
+        </button> */}
       </div>
     </div>
   );
